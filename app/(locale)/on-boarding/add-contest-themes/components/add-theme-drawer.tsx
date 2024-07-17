b797c676c03a87a0f427226d296e5ccc0640b68f
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-07-17 10:20:56 +0200

    add theme page

---
+++ app/(locale)/on-boarding/add-contest-themes/components/add-theme-drawer.tsx
@@ -0,0 +1,129 @@
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger
+} from "@/components/ui/drawer";
+import {Button} from "@/components/ui/button";
+import {PlusIcon} from "lucide-react";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import useContestStore from "@/domain/useContestStore";
+
+const formSchema = z.object({
+    name: z.string().min(2, {
+        message: "Le nom de jeu doit contenir au moins 2 caractères.",
+    }),
+    icon: z.string()
+})
+export default function AddThemeDrawer() {
+    const {themes, setThemes} = useContestStore()
+
+    const [openDrawer, setOpenDrawer] = useState(false)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            icon: 'SwatchBookIcon'
+        },
+    })
+
+    function onAddThemeSubmit(values: z.infer<typeof formSchema>) {
+        setThemes([...themes, {name: values.name, icon: lucideStringToIcon(values.icon).icon, selected: true}])
+        setOpenDrawer(false)
+    }
+
+    function lucideStringToIcon(icon: string) {
+        const Icon = require("lucide-react")[icon]
+        return {
+            icon: Icon,
+            iconJSX: <Icon/>
+        }
+    }
+
+    return <Drawer open={openDrawer} onOpenChange={setOpenDrawer}>
+        <DrawerTrigger asChild>
+            <Button className={"h-12"} variant={'ghost'}>
+                <div className={"flex gap-2 items-center"}>
+                    <PlusIcon/>
+                    <span className={"text-gray-800 "}>
+                                    Ajouter un thème
+                                </span>
+                </div>
+            </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+            <div className="mx-auto w-full max-w-sm">
+                <DrawerHeader>
+                    <DrawerTitle>Ajouter un thème</DrawerTitle>
+                    <DrawerDescription>Crééz votre thème et associez une icône</DrawerDescription>
+                </DrawerHeader>
+                <div className="p-4 pb-0">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onAddThemeSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="ex: Famille, Création artistique" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="icon"
+                                render={({field}) => (
+                                    <FormItem className={'w-max'}>
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <div className={'grid grid-cols-2 gap-2 items-center'}>
+                                                    <FormLabel>Associez une icône</FormLabel>
+                                                    <SelectTrigger>
+
+                                                        <SelectValue/>
+                                                    </SelectTrigger>
+                                                </div>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem
+                                                    value="SwatchBookIcon">
+                                                    {lucideStringToIcon('SwatchBookIcon').iconJSX}
+                                                </SelectItem>
+                                                <SelectItem
+                                                    value="CookingPotIcon">{lucideStringToIcon('CookingPot').iconJSX}</SelectItem>
+                                                <SelectItem
+                                                    value="SmileIcon">{lucideStringToIcon('SmileIcon').iconJSX}</SelectItem>
+                                                <SelectItem
+                                                    value="FerrisWheelIcon">{lucideStringToIcon('FerrisWheelIcon').iconJSX}</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
+                </div>
+                <DrawerFooter className={'grid grid-cols-2 gap-2'}>
+                    <Button onClick={() => setOpenDrawer(false)} variant="outline">Annuler</Button>
+                    <Button onClick={form.handleSubmit(onAddThemeSubmit)}>Valider</Button>
+                </DrawerFooter>
+            </div>
+        </DrawerContent>
+    </Drawer>
+
+}
--- app/(locale)/on-boarding/add-contest-themes/page.tsx
+++ app/(locale)/on-boarding/add-contest-themes/page.tsx
@@ -6,27 +6,32 @@ import {UndoIcon} from "lucide-react";
 import useContestStore from "@/domain/useContestStore";
 import {toast} from "@/components/hooks/use-toast";
 import {cn} from "@/lib/utils";
+import AddThemeDrawer from "@/app/(locale)/on-boarding/add-contest-themes/components/add-theme-drawer";
 
 
 export default function AddContestThemes() {
     const router = useRouter()
     const {themes, setThemes} = useContestStore()
 
+
     function onSubmit() {
         const numSelectedThemes = themes.filter(theme => theme.selected).length
-        if(numSelectedThemes < 1) {
-            toast({title : 'Veuillez sélectionner au moins un thème'})
+        if (numSelectedThemes < 1) {
+            toast({title: 'Veuillez sélectionner au moins un thème'})
             return;
         }
         setThemes(themes)
         router.push('/on-boarding/publish-contest')
     }
 
+
     function toggleTheme(index: number) {
         const newThemes = [...themes]
         newThemes[index].selected = !newThemes[index].selected
         setThemes(newThemes)
     }
+
+
 
     return (<>
             <Button className={'w-max'} onClick={() => router.push('/on-boarding/add-contest-description')}
@@ -37,21 +42,21 @@ export default function AddContestThemes() {
             <p className={"leading-7 text-gray-800"}>
                 {"Ajoutez des thèmes pour votre concours. Les participants pourront soumettre des photos en fonction de ces thèmes."}
             </p>
-        <div className={"grid grid-cols-2 gap-2"}>
-            {themes.map((theme, index) => (
-
-                <Button className={cn("h-12 ", theme.selected && 'border border-primary')} variant={theme.selected ? 'secondary' : 'outline'} onClick={() => toggleTheme(index)}
-                        key={index}>
-                    <div className={"flex gap-2 items-center"}>
-                    <theme.icon />
-                    <span className={"text-gray-800 italic "}>
+            <div className={"grid grid-cols-2 gap-2"}>
+                {themes.map((theme, index) => (
+                    <Button className={cn("h-12 ", theme.selected && 'border border-primary')}
+                            variant={theme.selected ? 'secondary' : 'outline'} onClick={() => toggleTheme(index)}
+                            key={index}>
+                        <div className={"flex gap-2 items-center"}>
+                            <theme.icon/>
+                            <span className={"text-gray-800 italic "}>
                         {theme.name}
                     </span>
-                    </div>
-                </Button>
-
-            ))}
-        </div>
+                        </div>
+                    </Button>
+                ))}
+                <AddThemeDrawer/>
+            </div>
             <div className={"grid gap-2 text-center py-4"}>
                 <Button type={"submit"} onClick={onSubmit} size={'lg'} variant={'default'}>Etape suivante</Button>
             </div>
