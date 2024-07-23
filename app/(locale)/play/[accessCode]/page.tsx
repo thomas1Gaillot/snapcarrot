47259119d39b0f974bb49855efd66de1fd7274bd
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-07-23 10:05:44 +0200

    admin user can change open phase to voting phase

--- app/(locale)/play/[accessCode]/page.tsx
+++ app/(locale)/play/[accessCode]/page.tsx
@@ -5,8 +5,10 @@ import VotePage from "@/app/(locale)/play/[accessCode]/components/vote/Vote";
 import ResultsPage from "@/app/(locale)/play/[accessCode]/components/results/Result";
 import {contestIcon, Status} from "@/domain/status/Status";
 import Image from "next/image";
+import useContestStore from "@/domain/contest/useContestStore";
 
 export default function PlayPage() {
+    const {status} = useContestStore()
     return <div className={"flex flex-col h-full "}>
         <Tabs defaultValue={"open"} className="w-full mt-8">
             <TabsList className="grid w-full grid-cols-3 mb-4">
@@ -20,14 +22,14 @@ export default function PlayPage() {
                 <TabsTrigger
                     value="vote"
                 >
-                    <Image alt={'vote-image'} src={contestIcon(Status.voting, false)}
+                    <Image alt={'vote-image'} src={contestIcon(Status.voting, status !== Status.open)}
                            width={48} height={48}
                            className="p-2 rounded flex items-center py-1"/>
                 </TabsTrigger>
                 <TabsTrigger
                     value="results"
                 >
-                    <Image alt={'results-image'} src={contestIcon(Status.results, false)}
+                    <Image alt={'results-image'} src={contestIcon(Status.results, status === Status.results)}
                            width={48} height={48}
                            className="p-2  rounded flex items-center py-1"/>
                 </TabsTrigger>
